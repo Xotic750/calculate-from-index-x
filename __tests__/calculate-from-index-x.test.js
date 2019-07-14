b96c9285b28619fdf5c0c0201111fb1ac0d715ae
Graham Fairweather <xotic750@gmail.com>
2019-07-14 11:54:30 +0200

    Terraformed
    Automated build

---
+++ __tests__/calculate-from-index-x.test.js
@@ -0,0 +1,101 @@
+let calcFromIndex;
+
+if (typeof module === 'object' && module.exports) {
+  require('es5-shim');
+  require('es5-shim/es5-sham');
+
+  if (typeof JSON === 'undefined') {
+    JSON = {};
+  }
+
+  require('json3').runInContext(null, JSON);
+  require('es6-shim');
+  const es7 = require('es7-shim');
+  Object.keys(es7).forEach(function(key) {
+    const obj = es7[key];
+
+    if (typeof obj.shim === 'function') {
+      obj.shim();
+    }
+  });
+  calcFromIndex = require('../../index.js');
+} else {
+  calcFromIndex = returnExports;
+}
+
+describe('calcFromIndex', function() {
+  it('is a function', function() {
+    expect(typeof calcFromIndex).toBe('function');
+  });
+
+  it('should throw when target not undefined', function() {
+    expect(function() {
+      calcFromIndex();
+    }).toThrow();
+
+    expect(function() {
+      calcFromIndex(void 0);
+    }).toThrow();
+
+    expect(function() {
+      calcFromIndex(null);
+    }).toThrow();
+  });
+
+  it('should return the correct values for array-like objects', function() {
+    const subjects = [
+      'abc',
+      [1, 2, 3],
+      {
+        0: 1,
+        1: 2,
+        2: 3,
+        length: 3,
+      },
+    ];
+
+    const values = [-Infinity, -1, 0, 1, Infinity, NaN, void 0, null, '', '10', false, true];
+
+    const exs = [0, 2, 0, 1, Infinity, 0, 0, 0, 0, 10, 0, 1];
+
+    const expected = subjects.map(function(subject) {
+      return values.map(function(value) {
+        return calcFromIndex(subject, value);
+      });
+    });
+
+    const actual = subjects.map(function() {
+      return exs;
+    });
+
+    expect(actual).toStrictEqual(expected);
+  });
+
+  it('should return the correct values non array-like objects', function() {
+    const subjects = [
+      1,
+      true,
+      function(a, b) {
+        return b;
+      },
+      /abc/i,
+      new Date(),
+    ];
+
+    const values = [-Infinity, -1, 0, 1, Infinity, NaN, void 0, null, '', '10', false, true];
+
+    const expected = subjects.map(function() {
+      return values.map(function() {
+        return 0;
+      });
+    });
+
+    const actual = subjects.map(function(subject) {
+      return values.map(function(value) {
+        return calcFromIndex(subject, value);
+      });
+    });
+
+    expect(actual).toStrictEqual(expected);
+  });
+});
--- src/calculate-from-index-x.js
+++ src/calculate-from-index-x.js
@@ -7,23 +7,21 @@
  * @module calculate-from-index-x
  */
 
-'use strict';
+const toObject = require('to-object-x');
+const toLength = require('to-length-x').toLength2018;
+const toInteger = require('to-integer-x').toInteger2018;
+const isArrayLike = require('is-array-like-x');
 
-var toObject = require('to-object-x');
-var toLength = require('to-length-x').toLength2018;
-var toInteger = require('to-integer-x').toInteger2018;
-var isArrayLike = require('is-array-like-x');
-
-var getMax = function _getMax(a, b) {
+const getMax = function _getMax(a, b) {
   return a >= b ? a : b;
 };
 
 /**
  * This method calculates a fromIndex of a given value for an array.
  *
- * @param {array} array * The array on which to calculate the starting index.
+ * @param {Array} array - * The array on which to calculate the starting index.
  * @throws {TypeError} If array is null or undefined.
- * @param {number} fromIndex * The position in this array at which to begin. A
+ * @param {number} fromIndex - * The position in this array at which to begin. A
  *  negative value gives the index of array.length + fromIndex by asc.
  * @returns {number} The calculated fromIndex. Default is 0.
  * @example
@@ -35,12 +33,13 @@ var getMax = function _getMax(a, b) {
  * calcFromIndex([1, 2, 3], -1); // 2
  */
 module.exports = function calcFromIndex(array, fromIndex) {
-  var object = toObject(array);
+  const object = toObject(array);
+
   if (isArrayLike(object) === false) {
     return 0;
   }
 
-  var index = toInteger(fromIndex);
+  const index = toInteger(fromIndex);
 
   return index >= 0 ? index : getMax(0, toLength(object.length) + index);
 };
