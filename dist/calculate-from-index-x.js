7dc2bb986dd3c92a3824aeb0d41959e3d61f2e86
Graham Fairweather <xotic750@gmail.com>
2019-09-05 21:56:26 +0200

    Maintenance

--- dist/calculate-from-index-x.js
+++ dist/calculate-from-index-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-09-04T23:46:37.525Z",
+  "date": "2019-09-05T19:56:20.547Z",
   "describe": "",
   "description": "Calculates a fromIndex of a given value for an array.",
   "file": "calculate-from-index-x.js",
